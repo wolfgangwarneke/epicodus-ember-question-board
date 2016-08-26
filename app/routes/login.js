4d53a92961059ff396bd3c94c3bc8f1da1695ca3
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-08-26 14:33:20 -0700

    complete login functionality for local service, but not update active status on user database yet

--- app/routes/login.js
+++ app/routes/login.js
@@ -6,21 +6,27 @@ export default Ember.Route.extend({
     userLogin(params) {
       var self = this;
       console.log(params);
-      var checkUser = this.store.findRecord('user', "-KQ7RksRyVqBwt0Trauf");
+      // var checkUser = this.store.findRecord('user', "-KQ7RksRyVqBwt0Trauf");
+      var checkUser = this.store.query('user', { orderBy: 'username', equalTo: params.username });
       // var checkUser = this.store.query('user', {
       //   orderBy: 'username',
       //   equalTo: "params.username"
       // });
       console.log(checkUser);
-      checkUser.then(function(returnedUser) {
-        console.log(returnedUser);
-        if (returnedUser.get('password') === params.password) {
-          self.get('logIn').login(returnedUser);
-          self.transitionTo('index');
-          console.log('true that');
-        };
-        console.log(returnedUser.get('password'));
-        console.log(params.password);
+      checkUser.then(function(returnedUserArray) {
+        if (returnedUserArray.get('length') !== 0) {
+          var returnedUser = returnedUserArray.get('firstObject');
+          if (returnedUser.get('password') === params.password) {
+            self.get('logIn').login(returnedUser);
+            self.transitionTo('index');
+            console.log('true that');
+          } else {
+            console.log('login failed');
+            self.transitionTo('login');
+          };
+        } else {
+          console.log('user not found');
+        }
       })
     }
   }
