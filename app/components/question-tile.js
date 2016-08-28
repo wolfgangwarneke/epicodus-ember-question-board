960ff21fc9d0b80190c84830cd6c243a332eb767
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-08-28 15:36:43 -0700

    add display of edit and delete buttons for questions on question main page if they belong to a logged in user

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  logIn: Ember.inject.service(),
+  loggedInUserMatch: Ember.computed('logIn.loggedInUser', 'question.user', function() {
+    var loggedInUser = this.get('logIn.loggedInUser.id');
+    var questionsUser = this.get('question.user.id');
+    console.log("Logged in user: " + loggedInUser);
+    console.log("questionsUser: " + questionsUser);
+    if (this.get('logIn.loggedInUser.id') === this.get('question.user.id')) {
+      return true;
+    } else {
+      return false;
+    }
+  }),
   contentPreview: Ember.computed('question.content', function() {
     var contentToShorten = this.get('question.content');
     if (contentToShorten.length > 50) {
