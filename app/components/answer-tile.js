d95b43dac2ec694d8035231917cf83854cfc0f45
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-08-28 18:01:00 -0700

    add functionality to edit an answer if it belongs to logged-in user

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  logIn: Ember.inject.service(),
+  loggedInUserMatch: Ember.computed('logIn.loggedInUser', 'answer.user', function() {
+    var loggedInUser = this.get('logIn.loggedInUser.id');
+    var answersUser = this.get('answer.user.id');
+    console.log("Logged in user: " + loggedInUser);
+    console.log("answersUser: " + answersUser);
+    if (this.get('logIn.loggedInUser.id') === this.get('answer.user.id')) {
+      return true;
+    } else {
+      return false;
+    }
+  }),
+  editing: false,
+  actions: {
+    edit(){
+      this.set('editing', true);
+    },
+    updateAnswer(answer) {
+      var newAnswer = answer;
+      newAnswer.save();
+      this.set('editing', false);
+    }
+  }
 });
